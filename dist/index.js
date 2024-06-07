0d84a9692aab1f7dd51c22680fc93b266425fded
nrhmedia <git@nrhmedia.com>
2024-06-07 18:19:41 +0700

    New

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,23 @@
 
   // src/index.ts
   $(document).ready(function() {
-    if (window.location.hash) {
+    function isBreakpointEnabled() {
+      const isDesktop = $("[loader-content='true']").attr("loader-on-desktop") === "true";
+      const isTablet = $("[loader-content='true']").attr("loader-on-tablet") === "true";
+      const isMobileLandscape = $("[loader-content='true']").attr("loader-on-mobile-landscape") === "true";
+      const isMobilePortrait = $("[loader-content='true']").attr("loader-on-mobile-portrait") === "true";
+      const width = $(window).width();
+      if (width >= 992 && !isDesktop)
+        return false;
+      if (width >= 768 && width < 992 && !isTablet)
+        return false;
+      if (width >= 480 && width < 768 && !isMobileLandscape)
+        return false;
+      if (width < 480 && !isMobilePortrait)
+        return false;
+      return true;
+    }
+    if (window.location.hash || !isBreakpointEnabled()) {
       $("[loader-content='true']").hide();
     } else {
       $("[loader-content='true']").show();
@@ -14,12 +30,16 @@
         "load",
         function load() {
           window.removeEventListener("load", load, false);
+          let triggerDelay = $("[loader-content='true']").attr("loader-trigger-delay");
+          let scrollDelay = $("[loader-content='true']").attr("loader-scroll-delay");
+          triggerDelay = parseInt(triggerDelay, 10);
+          scrollDelay = parseInt(scrollDelay, 10);
           setTimeout(function() {
             $("[loader-trigger='true']").click();
-          }, 2e3);
+          }, triggerDelay);
           setTimeout(function() {
             $("body").removeClass("lock-scroll-loader");
-          }, 2e3);
+          }, scrollDelay);
         },
         false
       );
