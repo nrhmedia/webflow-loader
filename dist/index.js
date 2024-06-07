31412bbb7f70400f1c4ec2578d047d2a642104e3
nrhmedia <git@nrhmedia.com>
2024-06-07 18:13:39 +0700

    Fixing pop?

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,49 @@
-"use strict";(()=>{$(document).ready(function(){window.location.hash?$("[loader-content='true']").hide():($("[loader-content='true']").show(),$("body").addClass("lock-scroll-loader"),window.addEventListener("load",function e(){window.removeEventListener("load",e,!1),setTimeout(function(){$("[loader-trigger='true']").click()},2e3),setTimeout(function(){$("body").removeClass("lock-scroll-loader")},2e3)},!1))});})();
+"use strict";
+(() => {
+  // bin/live-reload.js
+  new EventSource(`${"http://localhost:3000"}/esbuild`).addEventListener("change", () => location.reload());
+
+  // src/index.ts
+  $(document).ready(function() {
+    function isBreakpointEnabled() {
+      const isDesktop = $("[loader-content='true']").attr("loader-on-desktop") === "true";
+      const isTablet = $("[loader-content='true']").attr("loader-on-tablet") === "true";
+      const isMobileLandscape = $("[loader-content='true']").attr("loader-on-mobile-landscape") === "true";
+      const isMobilePortrait = $("[loader-content='true']").attr("loader-on-mobile-portrait") === "true";
+      const width = $(window).width();
+      if (width >= 992 && !isDesktop)
+        return false;
+      if (width >= 768 && width < 992 && !isTablet)
+        return false;
+      if (width >= 480 && width < 768 && !isMobileLandscape)
+        return false;
+      if (width < 480 && !isMobilePortrait)
+        return false;
+      return true;
+    }
+    if (window.location.hash || !isBreakpointEnabled()) {
+      $("[loader-content='true']").hide();
+    } else {
+      $("[loader-content='true']").show();
+      $("body").addClass("lock-scroll-loader");
+      window.addEventListener(
+        "load",
+        function load() {
+          window.removeEventListener("load", load, false);
+          let triggerDelay = $("[loader-content='true']").attr("loader-trigger-delay");
+          let scrollDelay = $("[loader-content='true']").attr("loader-scroll-delay");
+          triggerDelay = parseInt(triggerDelay, 10);
+          scrollDelay = parseInt(scrollDelay, 10);
+          setTimeout(function() {
+            $("[loader-trigger='true']").click();
+          }, triggerDelay);
+          setTimeout(function() {
+            $("body").removeClass("lock-scroll-loader");
+          }, scrollDelay);
+        },
+        false
+      );
+    }
+  });
+})();
+//# sourceMappingURL=index.js.map
